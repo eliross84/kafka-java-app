2bf8399c920e9d61c37cf605cefa9d847474853d
manideepchamala <54381062+manideepchamala@users.noreply.github.com>
2020-05-20 15:46:39 -0500

    created a custom producer(ManideepProducer.java)

---
+++ src/main/java/edu/nwmissouri/bigdata3/simple/ManideepProducer.java
@@ -0,0 +1,53 @@
+package edu.nwmissouri.bigdata3.simple;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ManideepProducer {
+  private static Scanner in;
+
+  public static void main(String[] argv) throws Exception {
+    if (argv.length != 1) {
+      System.err.println("Please specify 1 parameter (the name of the topic)");
+      System.exit(-1);
+    }
+    String topicName = argv[0];
+    in = new Scanner(System.in);
+    System.out.println("Thank you for providing the topic " + topicName + "\n");
+    System.out.println("Enter message (type exit to quit).\n");
+
+    // Configure the Producer
+    Properties configProperties = new Properties();
+    configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.ByteArraySerializer");
+    configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+    System.out.println("The configuration properties are: " + configProperties.toString());
+    System.out.println("\nWill use this configuration to create a producer.\n");
+
+    org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+
+    // My Custom logic to return first word of the input string from producer(based on space)
+    // It allows input from keyboard
+
+    String data = in.nextLine();
+    while (!data.equals("exit")) {
+      ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, data.split(" ")[0]);
+      producer.send(rec);
+      data = in.nextLine();
+    }
+
+
+
+    in.close();
+    producer.close();
+  }
+}
+
+
